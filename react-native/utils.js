86b27d1b184a77ec807e2383b57da776b8b54698
xulihang <xulihanghai@163.com>
2023-11-07 15:32:57 +0800

    use URL params to control the app

--- react-native/utils.js
+++ react-native/utils.js
@@ -53,3 +53,14 @@ function getRectFromPoints(points) {
     throw new Error("Invalid number of points");
   }
 }
+
+function getURLParameter(key) {
+  let paramString = window.location.href.split('?')[1];
+  let queryString = new URLSearchParams(paramString);
+  for(let pair of queryString.entries()) {
+    if (pair[0] === key) {
+      return pair[1];
+    }
+  }
+  return undefined;
+}
